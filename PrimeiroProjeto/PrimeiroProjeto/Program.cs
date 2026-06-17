3dc79141d3f7103c5f5458cbcba2b6f1eb7d551e
unknown <m.v.paixaoo@gmail.com>
2026-06-16 21:59:29 -0300

    ...

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+                Console.ReadKey();
+            }
+        }
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Cadastro de Clientes");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("1- Cadastrar cliente");
+            Console.WriteLine("2- Exibir cliente");
+            Console.WriteLine("3- Editar cliente");
+            Console.WriteLine("4- Excluir cliente");
+            Console.WriteLine("5- Sair");
+            Console.WriteLine("---------------------");
+        }
+    }
+}
